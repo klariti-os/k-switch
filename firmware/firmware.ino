3c0979e5bb2b5ffce3dc688628c86296312fb213
ignasxv <62844231+ignasxv@users.noreply.github.com>
2026-04-17 15:22:55 -0400

    feat: updated firmware

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,51 +1,41 @@
-/*
-  Blink
-
-  Turns an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://docs.arduino.cc/hardware/
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  https://docs.arduino.cc/built-in-examples/basics/Blink/
-*/
-
-// the setup function runs once when you press reset or power the board
-
 #include <WiFi.h>
+#include <HTTPClient.h>
 
 #define LED_BUILTIN 10
 
+const char* SSID     = "drena";
+const char* PASSWORD = "Chulio123";
+const char* TOGGLE_URL = "https://k-switch.vercel.app/state/toggle";
+
 void setup() {
   Serial.begin(115200);
-  Serial.println("Test");
   pinMode(LED_BUILTIN, OUTPUT);
 
-  Serial.println("Before WiFi.mode");
   WiFi.mode(WIFI_STA);
-  Serial.println("After WiFi.mode");
-  Serial.print("MAC Address: ");
-  Serial.println(WiFi.macAddress());
+  WiFi.begin(SSID, PASSWORD);
+
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.print("Connected, IP: ");
+  Serial.println(WiFi.localIP());
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // change state of the LED by setting the pin to the HIGH voltage level
-  delay(300);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // change state of the LED by setting the pin to the LOW voltage level
-  delay(300);      
-                  // wait for a second
-  Serial.println(WiFi.macAddress());
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    http.begin(TOGGLE_URL);
+    http.addHeader("Content-Type", "application/json");
+    int code = http.POST("");
+    Serial.printf("POST /state/toggle -> %d\n", code);
+    http.end();
+  } else {
+    Serial.println("WiFi disconnected, reconnecting...");
+    WiFi.reconnect();
+  }
+
+  delay(2000);
 }
